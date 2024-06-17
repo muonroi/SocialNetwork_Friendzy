c5f15e13c512ac89f7c1033fce24b279566834ab
muonroi <leanhphi1706@gmail.com>
2024-06-17 12:59:39 +0700

    add mongo docker

---
+++ src/Services/Message.Service/Message.Domain/Entities/AuthorEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Message.Domain.Entities
+{
+    public class AuthorEntity(string firstName, string id, string lastName, string imageUrl) :
+    {
+        public string FirstName { get; set; } = firstName;
+        public string Id { get; set; } = id;
+        public string LastName { get; set; } = lastName;
+        public string ImageUrl { get; set; } = imageUrl;
+    }
+}
---
+++ src/Services/Message.Service/Message.Domain/Entities/MessageEntity.cs
@@ -0,0 +1,31 @@
+﻿namespace Message.Domain.Entities
+{
+    public class MessageEntity(
+        AuthorEntity author,
+        long createdAt,
+        string id,
+        string status,
+        string text,
+        string type,
+        int height,
+        string name,
+        int size, string uri,
+        int width,
+        string mimeType,
+        TimeSpan? duration = null)
+    {
+        public AuthorEntity Author { get; set; } = author;
+        public long CreatedAt { get; set; } = createdAt;
+        public string Id { get; set; } = id;
+        public string Status { get; set; } = status;
+        public string Text { get; set; } = text;
+        public string Type { get; set; } = type;
+        public int Height { get; set; } = height;
+        public string Name { get; set; } = name;
+        public int Size { get; set; } = size;
+        public string Uri { get; set; } = uri;
+        public int Width { get; set; } = width;
+        public string MimeType { get; set; } = mimeType;
+        public TimeSpan? Duration { get; set; } = duration;
+    }
+}
